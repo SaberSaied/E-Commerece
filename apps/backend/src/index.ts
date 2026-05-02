99b1cf36f55b429a15cde52b129ef79971201a83
SaberSaied <sabersaied992003@gmail.com>
2026-05-02 20:12:48 +0300

    build first backend init and installed dependencies and authenticaion database

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,16 @@
+
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+
+const app = express();
+const port = 3000;
+app.use(cors());
+app.use(helmet());
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
